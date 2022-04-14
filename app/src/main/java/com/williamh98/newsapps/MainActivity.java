69ab1f371210ad389e178290d460baca639347f2
WilliamH98 <43578142+WilliamH98@users.noreply.github.com>
2022-04-14 09:21:45 +0700

    Add files via upload

---
+++ app/src/main/java/com/williamh98/newsapps/MainActivity.java
@@ -0,0 +1,14 @@
+package com.williamh98.newsapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
